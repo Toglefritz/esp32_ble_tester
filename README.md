b921db2219792d7fefc7e2a03f954f1fbaebf1f7
Scott Hatfield <contact@hatfieldscott.com>
2023-08-08 21:20:59 -0500

    Added support for Adafruit QT Py ESP32 C3 :pie:

--- README.md
+++ README.md
@@ -1,22 +1,22 @@
-# ESP32 BeeS3 Bluetooth Tester
+# ESP32 Bluetooth Tester
 
 ## Overview
-This project provides firmware for the ESP32 running on the Bee S3 development board. It serves as a simple tool to test external systems that utilize Bluetooth Low Energy (BLE) communication to control IoT devices. The sketch offers a BLE interface to control the Neopixel LED onboard the BeeS3 dev board by using two different characteristics.
+This project provides firmware for the ESP32 equipped with a Neopixel LED. It serves as a simple tool to test external systems that utilize Bluetooth Low Energy (BLE) communication to control IoT devices. The sketch offers a BLE interface to control the Neopixel LED by using two different characteristics.
 
 ## Features
 - Automatic BLE advertisement on startup
-- Control of a Neopixel LED onboard the BeeS3 dev board through BLE characteristics
+- Control of a Neopixel LED connected to the dev board through BLE characteristics
 - Open characteristic for green LED control
 - Encrypted characteristic for red LED control
 
 ## Libraries Used
 - ESP32 BLE Arduino Library
-- BEES3 Library (for controlling the Neopixel LED)
+- Adafruit Neopixel Library
 
 ## Getting Started
 ### Prerequisites
 - PlatformIO installed in Visual Studio Code
-- BeeS3 development board with an ESP32 chip
+- Development board with an ESP32 chip and either an onboard Neopixel LED or an external Neopixel LED connected to a GPIO pin
 - USB cable to connect the development board to your computer
 
 ### Upload Instructions
@@ -30,7 +30,7 @@ board = esp32dev
 upload_port = YOUR_COM_PORT
 ```
 
-3. Connect BeeS3 Board: Connect the BeeS3 development board to your computer using the USB cable.
+3. Connect ESP32 Board: Connect the development board to your computer using the USB cable.
 4. Build the Sketch: Click the "Build" button (a checkmark icon) at the bottom of the VS Code window.
 5. Upload the Sketch: Click the "Upload" button (a right arrow icon) at the bottom of the VS Code window. This will compile the sketch and upload it to the connected ESP32 device.
 6. Monitor Serial Output: Click the "Serial Monitor" button (a plug icon) at the bottom of the VS Code window to open the serial monitor and view the log output from the ESP32.
--- esp32_bluetooth_tester/src/main.cpp
+++ esp32_bluetooth_tester/src/main.cpp
@@ -1,18 +1,18 @@
 /**
  * @file
- * @brief Firmware for the ESP32 running on the Bee S3 development board.
+ * @brief Firmware for the ESP32 with a Neopixel LED.
  *
  * This firmware acts as a simple tool for testing external systems that
  * utilize Bluetooth Low Energy (BLE) communication to control IoT devices.
  * It provides two main functionalities:
  *   1. Automatically advertise the BLE capabilities of the ESP32 and manage
  *      connections and disconnections of BLE clients.
- *   2. Control the Neopixel LED onboard the BeeS3 dev board through BLE.
+ *   2. Control the Neopixel LED connected to the ESP32 dev board through BLE.
  *      It has two characteristics:
  *        - An open characteristic to turn the LED on to a green color.
  *        - An encrypted characteristic to turn the LED on to a red color.
  *
- * The BEES3 library is used to control the Neopixel LED, and the ESP32 BLE
+ * The Adafruit Neopixel library is used to control the Neopixel LED, and the ESP32 BLE
  * Arduino library is used for BLE communication.
  *
  * This implementation is designed to be a practical testing tool to validate
@@ -24,10 +24,22 @@
 #include <BLEServer.h>
 #include <BLEUtils.h>
 #include <BLE2902.h>
-#include <BeeS3.h>
+#include <Adafruit_NeoPixel.h>
 
-// Pull the BEES3 class from the helper and name it bees3 so we can use it below.
-BEES3 bees3;
+/*
+  SETUP: Uncomment one of the define statements below depending on the hardware configuration for your
+         ESP32 dev board. If your board is not listed, add another define statement based on the
+         PIN to which the onboard or external Neopixel LED is wired.
+*/
+
+// On the Bee S3, the onboard Neopixel LED is connected to GPIO48
+//#define LED 48
+
+// On the Adafruit QT Py ESP32 C3, the Neopixel LED is connected to GPIO2
+#define LED 2
+
+// Initalize the Neopixel library used to control the LED.
+Adafruit_NeoPixel led = Adafruit_NeoPixel(1, LED, NEO_GRB + NEO_KHZ800);
 
 /**
  * @class ServerCallbacks
@@ -79,16 +91,19 @@ class Callbacks : public BLECharacteristicCallbacks {
         std::string value = pCharacteristic->getValue();
         if (value == "ON") {
             if (pCharacteristic->getUUID().equals(BLEUUID("abcd1234-1234-1234-1234-1234567890ab"))) {
-                bees3.setPixelColor(green); // Green LED
+                led.setPixelColor(0, led.Color(0, 255, 0)); // Green LED
+                led.show(); 
                 Serial.println("Green LED ON");
             }
             else if (pCharacteristic->getUUID().equals(BLEUUID("abcd1234-1234-1234-1234-1234567890ac"))) {
-                bees3.setPixelColor(red); // Red LED
+                led.setPixelColor(0, led.Color(255, 0, 0)); // Red LED
+                led.show(); 
                 Serial.println("Red LED ON");
             }
         }
         else if (value == "OFF") {
-            bees3.setPixelColor(off);
+            led.setPixelColor(0, led.Color(0, 0, 0)); // Off
+            led.show(); 
             Serial.println("LED OFF");
         } 
         else {
@@ -99,11 +114,14 @@ class Callbacks : public BLECharacteristicCallbacks {
 };
 
 void setup() {
-    // Initalize the helper library.
-    bees3.begin();
+    // Initialize the LED via the Neopixel library
+    led.begin();
 
-    // Brightness is 0-255. We set it to 1/2 brightness here.
-    bees3.setPixelBrightness(255 / 2);
+    // Set the LED brightness
+    led.setBrightness(50);
+
+    // Initialize the LED s to 'off'
+    led.show(); 
 
     Serial.begin(115200);
 
